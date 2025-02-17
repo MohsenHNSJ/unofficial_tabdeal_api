dedbbc3d4b4e69a40aad105f3f6db0f3d7c81c4c
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2025-02-17 10:11:10 +0330

    Remove unnecessary configuration options from Sphinx documentation settings

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@ copyright = "2025, MohsenHNSJ"
 version = release = "0.1.2"
 needs_sphinx = "8.1.3"
 highlight_language = "python3"
-nitpicky = True
 language = "en"
 extensions = [
     "myst_parser",
@@ -24,5 +23,4 @@ extensions = [
     "sphinx.ext.duration",
 ]
 napoleon_google_docstring = True
-napoleon_include_private_with_doc = True
 html_theme = "furo"
