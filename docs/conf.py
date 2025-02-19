fd618e11503886629c56a0752e0cd75fb3593932
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2025-02-19 21:56:52 +0330

    Fixed errors

--- docs/conf.py
+++ docs/conf.py
@@ -14,17 +14,16 @@ version = release = "0.1.2"
 needs_sphinx = "8.1.3"
 highlight_language = "python3"
 language = "en"
-suppress_warnings = ()
 
 extensions = [
     "myst_parser",
     "sphinx.ext.imgconverter",
     "sphinx.ext.duration",
     "versionwarning.extension",
-    "hoverxref.extension",
 ]
 
 # HoverXref
+extensions += ["hoverxref.extension"]
 # show a tooltip in all the appearances of the :ref: role
 hoverxref_auto_ref = True
 hoverxref_intersphinx = [
@@ -34,6 +33,10 @@ hoverxref_intersphinx = [
 hoverxref_domains = [
     "py",
 ]
+# Defining role type to mitigate >>> Using default style (tooltip) for unknown typ (obj). Define it in hoverxref_role_types.
+hoverxref_role_types = {
+    'obj': 'tooltip'
+}
 
 # AutoAPI
 extensions += ["autoapi.extension"]
