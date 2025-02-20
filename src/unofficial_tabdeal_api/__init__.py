5240c481bedac03f940b54103d50ac8876fff007
Mohsen <97420292+MohsenHNSJ@users.noreply.github.com>
2025-02-20 11:41:54 +0330

    Enhance module docstring and add metadata for unofficial Tabdeal API

--- src/unofficial_tabdeal_api/__init__.py
+++ src/unofficial_tabdeal_api/__init__.py
@@ -1,3 +1,23 @@
-"""Initializer"""
+"""
+Unofficial Tabdeal API
+----------------------
 
-from .tabdeal_client import TabdealClient
+a Package to communicate with Tabdeal platform
+
+:copyright: (c) 2025-present MohsenHNSJ
+:license: MIT, see LICENSE for more details
+
+"""
+
+__title__ = "unofficial-tabdeal-api"
+__author__ = "MohsenHNSJ"
+__license__ = "MIT"
+__copyright__ = "Copyright 2025-present MohsenHNSJ"
+__version__ = "0.1.2"
+
+from .authorization import *
+from .base import *
+from .constants import *
+from .margin import *
+from .tabdeal_client import *
+from .utils import *
