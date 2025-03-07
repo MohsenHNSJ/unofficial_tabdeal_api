b5e48383dd1123945eb6d69c3a9b1b01283f5ad7
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-03-07 02:04:02 +0000

    Added module docstring Added method docstring Added type hinting

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,15 @@
+"""This file is a Python file that defines a set of sessions."""
 import nox
+
 
 # Session for checking and linting the code
 @nox.session
-def lint(session):
+def lint(session: nox.session.Session) -> None:
+    """Lints the code with Flake8 and Ruff
+
+    Args:
+        session (nox.session.Session): An environment and a set of commands to run in that environment.
+    """
     # Install requirements
     session.install("flake8", "flake8-quotes")
     # Show version of running package
@@ -11,4 +18,3 @@ def lint(session):
     session.run("flake8", "tests/", "--color", "always")
     session.run("flake8", "src/", "--color", "always")
     session.run("flake8", "docs/", "--color", "always")
-    
