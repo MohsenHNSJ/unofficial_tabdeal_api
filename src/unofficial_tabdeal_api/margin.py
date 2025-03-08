6b59d980ad7be526d5bcb4a3fbd4f4774c56cf45
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-03-08 00:39:34 +0000

    Fixed linting problems

--- src/unofficial_tabdeal_api/margin.py
+++ src/unofficial_tabdeal_api/margin.py
@@ -1,6 +1,7 @@
-"""This module holds the MarginClass"""
+"""This module holds the MarginClass."""
 
 from typing import Any
+
 from unofficial_tabdeal_api.base import BaseClass
 from unofficial_tabdeal_api.constants import (
     GET_ALL_MARGIN_OPEN_ORDERS_URL,
@@ -10,21 +11,22 @@ from unofficial_tabdeal_api.constants import (
 
 
 class MarginClass(BaseClass):
-    """This is the class storing methods related to Margin trading"""
+    """This is the class storing methods related to Margin trading."""
 
     async def get_margin_asset_id(self, isolated_symbol: str) -> int:
-        """Gets the ID of a margin asset from server and returns it as an integer
+        """Gets the ID of a margin asset from server and returns it as an integer.
 
         Returns -1 in case of an error
 
         Args:
-            isolated_symbol (str): Isolated symbol of margin asset. example: BTCUSDT, MANAUSDT, BOMEUSDT, ...
+            isolated_symbol (str): Isolated symbol of margin asset.
+            example: BTCUSDT, MANAUSDT, BOMEUSDT, ...
 
         Returns:
             int: Margin asset ID as integer
         """
-
-        self._logger.debug('Trying to get margin asset ID for [%s]', isolated_symbol)
+        self._logger.debug(
+            'Trying to get margin asset ID for [%s]', isolated_symbol)
 
         connection_url: str = (
             GET_MARGIN_ASSET_DETAILS_PRT1
@@ -55,14 +57,13 @@ class MarginClass(BaseClass):
         return margin_asset_id
 
     async def get_all_open_margin_orders(self) -> list[dict[str, Any]] | None:
-        """Gets all the open margin orders from server and returns it as a list of dictionaries
+        """Gets all the open margin orders from server and returns it as a list of dictionaries.
 
         Returns `None` in case of an error
 
         Returns:
             list[dict[str, Any]] | None: a List of dictionary items or `None` in case of an error
         """
-
         self._logger.debug('Trying to get all open margin orders')
 
         # We get the data from server and save it in a temporary variable
