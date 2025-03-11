0cc89a41ab380422867b0a8ccf95eb80c9686675
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-03-11 03:23:53 +0000

    Disabled the re-using of environments Added package name Removed docs_build_requirements, instead uses the requirements file in docs folder Removed showing the python version (redundant) Added some comments Fixed docs_build session Refactor docs_preview to use docs_build session Added mypy_check session

--- noxfile.py
+++ noxfile.py
@@ -4,9 +4,7 @@ from pathlib import Path
 
 import nox
 
-# Documentation build requirements
-doc_build_requirements = ["sphinx", "sphinx-autobuild", "furo", "sphinx-autoapi",
-                          "sphinx-hoverxref", "sphinx-notfound-page", "sphinx-version-warning"]
+package_name: str = "unofficial_tabdeal_api"
 # Minimum nox required
 nox.needs_version = ">=2025.02.09"
 # Sessions default backend
@@ -16,7 +14,7 @@ nox.options.default_venv_backend = "conda"
 nox.options.sessions = []
 
 
-@nox.session(venv_backend="conda", python=["3.13"], reuse_venv=True, tags=["check"])
+@nox.session(venv_backend="conda", python=["3.13"], tags=["check"])
 def ruff_check(session: nox.sessions.Session) -> None:
     """Check the code with Ruff.
 
@@ -26,9 +24,8 @@ def ruff_check(session: nox.sessions.Session) -> None:
     # Install requirements
     session.run("pip", "install", "--upgrade", "ruff", silent=True)
     # Show version
-    session.run("python", "--version")
     session.run("ruff", "version")
-    # If argument is provided, append to command
+    # If argument is provided, append to command to fix errors
     if session.posargs:
         # Join the characters of input argument into a single string
         argument = "".join(session.posargs)
@@ -38,33 +35,78 @@ def ruff_check(session: nox.sessions.Session) -> None:
         session.run("ruff", "check")
 
 
-@nox.session(venv_backend="conda", python=["3.13"], reuse_venv=True, tags=["fix"])
+@nox.session(venv_backend="conda", python=["3.13"], tags=["fix"])
 def ruff_fix(session: nox.sessions.Session) -> None:
     """Fixes the code with Ruff.
 
     Args:
         session (nox.sessions.Session): An environment and a set of commands to run.
     """
+    # Run ruff_check session with autofix
     session.notify("ruff_check", "--fix")
 
 
-@nox.session(venv_backend="conda", python=["3.13"], reuse_venv=True, tags=["preview"])
-def preview_docs(session: nox.sessions.Session) -> None:
-    """Build and serve the documentation with live reloading on file changes.
+@nox.session(venv_backend="conda", python=["3.13"], tags=["docs"])
+def docs_build(session: nox.sessions.Session) -> None:
+    """Build the documentation.
 
     Args:
         session (nox.sessions.Session): An environment and a set of commands to run.
     """
-    args = ["--open-browser", "docs", "docs/_build"]
+    # Sphinx default arguments
+    arguments = ["docs", "docs/_build"]
+    # Make output colorful
+    arguments.append("--color")
+    # Show only warnings and errors
+    arguments.append("--quiet")
     # Install the package
     session.install(".")
     # Install requirements
-    session.run("pip", "install", "--upgrade",
-                *doc_build_requirements, silent=True)
+    session.run("pip", "install", "--upgrade", "-r",
+                "docs/requirements.txt", silent=True)
     # Set build path
     build_dir = Path("docs", "_build")
     # If build path exists, clear it
     if build_dir.exists():
         shutil.rmtree(build_dir)
-    # Run documentation building
-    session.run("sphinx-autobuild", *args)
+    # If argument is provided, run autobuild
+    if session.posargs:
+        # Join the characters of input argument into a single string
+        argument = "".join(session.posargs)
+        # Add the argument to beginning of the list
+        arguments.insert(0, argument)
+        # Run documentation build and live preview
+        session.run("sphinx-autobuild", *arguments)
+    else:
+        # Run documentation build only
+        session.run("sphinx-build", *arguments)
+
+
+@nox.session(venv_backend="conda", python=["3.13"], tags=["preview"])
+def docs_preview(session: nox.sessions.Session) -> None:
+    """Build and serve the documentation with live reloading on file changes.
+
+    Args:
+        session (nox.sessions.Session): An environment and a set of commands to run.
+    """
+    # Run documentation build with live preview
+    session.notify("docs_build", "--open-browser")
+
+
+@nox.session(venv_backend="conda", python=["3.13"], tags=["type"])
+def mypy_check(session: nox.sessions.Session) -> None:
+    """Type check using MyPy.
+
+    Args:
+        session (nox.sessions.Session): An environment and a set of commands to run.
+    """
+    # MyPy default check locations
+    arguments = ["src", "tests", "docs/conf.py"]
+    # Install the package
+    session.install(".")
+    # Install requirements
+    session.run("pip", "install", "--upgrade", "mypy", "pytest", silent=True)
+    # Show version
+    session.run("mypy", "--version")
+    # Run MyPy type checking
+    session.run("mypy", *arguments)
