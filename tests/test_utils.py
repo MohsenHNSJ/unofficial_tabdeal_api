b56f00608eae523573059ac8726316f6f2e2e01b
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-03-17 05:56:48 +0000

    Added async test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,29 @@
 """This module is for testing the functions of utils module."""
 # ruff: noqa: S101
 
-from unofficial_tabdeal_api.utils import create_session_headers
+import pytest
+
+from unofficial_tabdeal_api.utils import create_session_headers, create_session_headers_async
 
 TEST_USER_HASH: str = "TEST_USER_HASH"
 TEST_USER_AUTH_KEY: str = "TEST_USER_AUTH_KEY"
 
+# region Expected_results
+expected_session_headers: dict[str, str] = {
+    "user-hash": TEST_USER_HASH,
+    "Authorization": TEST_USER_AUTH_KEY,
+}
+# endregion Expected_results
+
 
 def test_create_session_headers() -> None:
     """Tests the function of create_session_headers."""
-    expected_result: dict[str, str] = {
-        "user-hash": TEST_USER_HASH,
-        "Authorization": TEST_USER_AUTH_KEY,
-    }
     assert create_session_headers(
-        TEST_USER_HASH, TEST_USER_AUTH_KEY) == expected_result
+        TEST_USER_HASH, TEST_USER_AUTH_KEY) == expected_session_headers
+
+
+@pytest.mark.asyncio
+async def test_create_session_headers_async() -> None:
+    """Tests the function of create_session_headers_async."""
+    result = await create_session_headers_async(TEST_USER_HASH, TEST_USER_AUTH_KEY)
+    assert result == expected_session_headers
