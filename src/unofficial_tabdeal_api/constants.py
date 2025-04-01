511c215cf471208f84d51cf50b105fd58a5c11ee
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-04-01 04:52:17 +0000

    Renamed constants

--- src/unofficial_tabdeal_api/constants.py
+++ src/unofficial_tabdeal_api/constants.py
@@ -2,7 +2,7 @@
 
 BASE_API_URL: str = "https://api.etctabdeal.org"
 
-GET_ACCOUNT_PREFERENCES_URL: str = "/r/preferences/"
+GET_ACCOUNT_PREFERENCES_URI: str = "/r/preferences/"
 """URL for getting account preferences. Used for checking authorization key validity"""
 
 # region Margin
@@ -12,7 +12,7 @@ The isolated_symbol of the margin asset is added between the two parts"""
 GET_MARGIN_ASSET_DETAILS_PRT2: str = "&account_genre=IsolatedMargin"
 """Seconds part of the URL for getting margin asset details
 The isolated_symbol of the margin asset is added between the two parts"""
-GET_ALL_MARGIN_OPEN_ORDERS_URL: str = "/r/treasury/isolated_positions/"
+GET_ALL_MARGIN_OPEN_ORDERS_URI: str = "/r/treasury/isolated_positions/"
 """URL for getting all open margin orders."""
 STATUS_OK: int = 200
 """The request succeeded"""
