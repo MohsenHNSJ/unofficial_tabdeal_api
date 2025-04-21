797fd9cf08a5c4b7014585d5529489e75f4e1e20
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-04-21 15:34:20 +0000

    Fully covered authorization.py

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -22,6 +22,7 @@ from tests.test_enums import HttpRequestMethod
 from tests.test_helper_functions import server_maker
 from unofficial_tabdeal_api.authorization import AuthorizationClass
 from unofficial_tabdeal_api.constants import GET_ACCOUNT_PREFERENCES_URI
+from unofficial_tabdeal_api.enums import DryRun
 
 
 async def test_is_authorization_key_valid(aiohttp_server, caplog: pytest.LogCaptureFixture) -> None:
@@ -70,8 +71,32 @@ async def test_keep_authorization_key_alive(
 ) -> None:
     """Tests the keep_authorization_key_alive function."""
     # Start web server
+    server: test_utils.TestServer = await server_maker(
+        aiohttp_server,
+        HttpRequestMethod.GET,
+        server_authorization_valid_responder,
+        GET_ACCOUNT_PREFERENCES_URI,
+    )
 
     # Check correct function
+    async with ClientSession(base_url=TEST_SERVER_ADDRESS) as client_session:
+        test_keep_alive_object: AuthorizationClass = AuthorizationClass(
+            TEST_USER_HASH,
+            TEST_USER_AUTH_KEY,
+            client_session,
+        )
+
+        with caplog.at_level(logging.DEBUG):
+            async with asyncio.TaskGroup() as task_group:
+                # Create a task for dry-run
+                keep_authorization_key_alive_dryrun_task = task_group.create_task(
+                    test_keep_alive_object.keep_authorization_key_alive(
+                        wait_time=1,
+                        dryrun=DryRun.YES,
+                    ),
+                )
+
+            assert "Authorization key is still valid." in caplog.text
 
     # Check error
     async with ClientSession(base_url=INVALID_SERVER_ADDRESS) as client_session:
