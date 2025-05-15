6bde33a1bdfeedf85dfb1908fbcea6795aada951
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-05-15 07:22:52 +0000

    Re-worked tests to reduce boilerplate code

--- tests/test_order.py
+++ tests/test_order.py
@@ -4,10 +4,10 @@
 # pylint: disable=W0613,W0612,C0301,W0212
 
 import logging
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
 import pytest
-from aiohttp import ClientSession
+from aiohttp import ClientSession, test_utils
 
 from tests.test_constants import (
     SAMPLE_GET_ORDERS_HISTORY_LIST,
@@ -25,30 +25,15 @@ from unofficial_tabdeal_api.constants import GET_ORDERS_HISTORY_URI
 from unofficial_tabdeal_api.enums import OrderState
 from unofficial_tabdeal_api.order import OrderClass
 
-# Unused imports add a performance overhead at runtime, and risk creating import cycles.
-# If an import is only used in typing-only contexts,
-# it can instead be imported conditionally under an if TYPE_CHECKING: block to minimize runtime overhead.
-if TYPE_CHECKING:
-    from aiohttp import test_utils
-
 
 async def test_get_orders_details_history(aiohttp_server, caplog: pytest.LogCaptureFixture) -> None:
     """Tests the _get_orders_details_history function."""
     # Start web server
-    server: test_utils.TestServer = await server_maker(
-        aiohttp_server=aiohttp_server,
-        http_request_method=HttpRequestMethod.GET,
-        function_to_call=server_get_responder,
-        uri_path=GET_ORDERS_HISTORY_URI,
-    )
+    server: test_utils.TestServer = await make_test_order_server(aiohttp_server)
 
     # Check correct request
     async with ClientSession(base_url=TEST_SERVER_ADDRESS) as client_session:
-        test_get_orders: OrderClass = OrderClass(
-            user_hash=TEST_USER_HASH,
-            authorization_key=TEST_USER_AUTH_KEY,
-            client_session=client_session,
-        )
+        test_get_orders: OrderClass = await make_test_order_object(client_session)
 
         with caplog.at_level(logging.DEBUG):
             response: list[dict[str, Any]] = await test_get_orders._get_orders_details_history(
@@ -62,23 +47,33 @@ async def test_get_orders_details_history(aiohttp_server, caplog: pytest.LogCapt
 async def test_get_order_state(aiohttp_server, caplog: pytest.LogCaptureFixture) -> None:
     """Tests the get_order_state function."""
     # Start web server
-    server: test_utils.TestServer = await server_maker(
-        aiohttp_server=aiohttp_server,
-        http_request_method=HttpRequestMethod.GET,
-        function_to_call=server_get_responder,
-        uri_path=GET_ORDERS_HISTORY_URI,
-    )
+    server: test_utils.TestServer = await make_test_order_server(aiohttp_server)
 
     # Check correct request
     async with ClientSession(base_url=TEST_SERVER_ADDRESS) as client_session:
-        test_get_order_status: OrderClass = OrderClass(
-            user_hash=TEST_USER_HASH,
-            authorization_key=TEST_USER_AUTH_KEY,
-            client_session=client_session,
-        )
+        test_get_order_status: OrderClass = await make_test_order_object(client_session)
 
         with caplog.at_level(logging.DEBUG):
             response: OrderState = await test_get_order_status.get_order_state(SAMPLE_ORDER_ID)
             assert response is OrderState.FILLED
         assert f"Getting order state for [{SAMPLE_ORDER_ID}]" in caplog.text
         assert f"Order [{SAMPLE_ORDER_ID}] is in [{response.name}] state" in caplog.text
+
+
+async def make_test_order_server(aiohttp_server) -> test_utils.TestServer:
+    """Creates the server for testing OrderClass."""
+    return await server_maker(
+        aiohttp_server=aiohttp_server,
+        http_request_method=HttpRequestMethod.GET,
+        function_to_call=server_get_responder,
+        uri_path=GET_ORDERS_HISTORY_URI,
+    )
+
+
+async def make_test_order_object(client_session: ClientSession) -> OrderClass:
+    """Creates a test object for testing OrderClass."""
+    return OrderClass(
+        user_hash=TEST_USER_HASH,
+        authorization_key=TEST_USER_AUTH_KEY,
+        client_session=client_session,
+    )
