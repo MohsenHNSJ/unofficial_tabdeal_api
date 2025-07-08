e577b524225007a267bfba56294e9634f193a36d
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-07-08 18:49:27 +0000

    Added `model` to init file to enable importing in production

--- src/unofficial_tabdeal_api/__init__.py
+++ src/unofficial_tabdeal_api/__init__.py
@@ -14,7 +14,7 @@ __license__ = "MIT"
 __copyright__ = "Copyright 2025-present MohsenHNSJ"
 __version__ = "0.3.4"
 
-from . import constants, enums, exceptions, utils
+from . import constants, enums, exceptions, models, utils
 from .authorization import AuthorizationClass
 from .base import BaseClass
 from .margin import MarginClass
@@ -32,5 +32,6 @@ __all__: list[str] = [
     "constants",
     "enums",
     "exceptions",
+    "models",
     "utils",
 ]
