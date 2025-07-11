7dfb23ded4acea38916c9764b1946022e190aed6
MohsenHNSJ <97420292+MohsenHNSJ@users.noreply.github.com>
2025-07-11 06:43:24 +0000

    Remove unused DryRun enum and related keep_authorization_key_alive function from authorization module and tests. Related to [🐛 Bug]:  `keep_authorization_key_alive` is actually useless Fixes #450

--- src/unofficial_tabdeal_api/enums.py
+++ src/unofficial_tabdeal_api/enums.py
@@ -3,15 +3,6 @@
 from enum import Enum
 
 
-class DryRun(Enum):
-    """Enum class to indicate whether to dry run or not."""
-
-    YES = 1
-    """Run as a Dry-run"""
-    NO = 2
-    """Don't run as a Dry-run"""
-
-
 class OrderState(Enum):
     """Enum class to indicate the state of order."""
 
